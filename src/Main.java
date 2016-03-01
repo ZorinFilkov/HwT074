a7e7d3ced19ce6ac0c4fb231d1360d6851e5b340
ZorinFilkov <zfilkov@abv.bg>
2016-03-01 13:14:28 +0200

    HwT074

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int j = 0, k = 30; j < 5; j++) {
+			for (int i = 0; i < 6; i++, k--) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+
+	}
+}
